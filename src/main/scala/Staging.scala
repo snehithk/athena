43a60a92f7ac0b6489470130f149b4a75b7634f7
snehith <ksnehith@live.com>
2020-08-23 03:02:22 -0400

    third commit

--- src/main/scala/Staging.scala
+++ src/main/scala/Staging.scala
@@ -6,9 +6,7 @@ import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 
 
 object Staging extends App {
-//  val  credentials:AWSCredentials = new BasicAWSCredentials("ASIAYWHDNYZDYM5YTGNX",
-//    "0pO5E2LmiR8xIxOi7M+St2FUYr1/e3dqmwD8vxAu",
-//  "FwoGZXIvYXdzEKX//////////wEaDE563sqTP4ot9e9zFiLEAaXde5SbikLSkMZiPZL0BkyS0t+LyGNimOKzV+UmyE60COVPW4gAAi6JjUyYzprTsELlMFtQ/dh8lIhPdExog/cYuuBUuPKjHwXcyO0GAJeq1Uns8N9kCCX3hECuSzfdIUtYXewlClNzOpgfyWf+LFguEU8wCCReWQo4JQM7EOcX2G33hAGfJA/tXs0S9isfomx1FgRq/Pa1mw/UEF4jia5u9fW5sSO8e4CutL764bU9lEjqyUVvC/n2l/Br6tv9GANCtwUow/GF+gUyLe75SZ7KW/Qobmhq3ORwcEDbsrE9wCGlLvoc6cVq+WlM2qAzOTsn65dotMlUig==")
+
   val s3client:AmazonS3 = AmazonS3ClientBuilder
     .standard()
     //.withCredentials(new AWSStaticCredentialsProvider(credentials))
